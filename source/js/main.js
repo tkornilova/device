7689a4594587153c0fddf12bb0b57490b7aa9ed9
tkornilova <kornilova.tat07@gmail.com>
2022-07-30 00:18:23 +0400

    About us

--- source/js/main.js
+++ source/js/main.js
@@ -1,46 +1,25 @@
-import {iosVhFix} from './utils/ios-vh-fix';
-import {initModals} from './modules/modals/init-modals';
+const buttonMore = document.querySelector('.aboutus__button');
+const hiddenInfo = document.querySelector('.container__info--hidden');
+const hiddenInfoSpan = document.querySelector('.container__info span');
 
-// ---------------------------------
+buttonMore.addEventListener('click', () => {
+  hiddenInfo.classList.toggle('hidden');
 
-window.addEventListener('DOMContentLoaded', () => {
+  if (hiddenInfoSpan.style.display === 'none') {
+    hiddenInfoSpan.style.display = 'none';
+  } else {
+    hiddenInfoSpan.style.display = 'initial';
+  }
 
-  // Utils
-  // ---------------------------------
-
-  iosVhFix();
-
-  // Modules
-  // ---------------------------------
-
-  // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
-  // в load следует добавить скрипты, не участвующие в работе первого экрана
-  window.addEventListener('load', () => {
-    initModals();
-  });
+  if (buttonMore.textContent === 'Подробнее') {
+    buttonMore.innerHTML = 'Свернуть';
+  } else {
+    buttonMore.innerHTML = 'Подробнее';
+  }
 });
 
-// ---------------------------------
-
-// ❗❗❗ обязательно установите плагины eslint, stylelint, editorconfig в редактор кода.
-
-// привязывайте js не на классы, а на дата атрибуты (data-validate)
-
-// вместо модификаторов .block--active используем утилитарные классы
-// .is-active || .is-open || .is-invalid и прочие (обязателен нейминг в два слова)
-// .select.select--opened ❌ ---> [data-select].is-open ✅
-
-// выносим все в дата атрибуты
-// url до иконок пинов карты, настройки автопрокрутки слайдера, url к json и т.д.
-
-// для адаптивного JS используейтся matchMedia и addListener
-// const breakpoint = window.matchMedia(`(min-width:1024px)`);
-// const breakpointChecker = () => {
-//   if (breakpoint.matches) {
-//   } else {
-//   }
-// };
-// breakpoint.addListener(breakpointChecker);
-// breakpointChecker();
-
-// используйте .closest(el)
+window.addEventListener('resize', () => {
+  if (window.screen.width < 767 && buttonMore.innerHTML === 'Свернуть') {
+    hiddenInfoSpan.style.display = 'initial';
+  }
+});
