eb856614f43d691f9a04798041da65ba2f2c1ae9
tkornilova <kornilova.tat07@gmail.com>
2022-07-31 21:53:48 +0400

    Footer

--- source/js/main.js
+++ source/js/main.js
@@ -1,3 +1,5 @@
+// Additional descripton
+
 const buttonMore = document.querySelector('.aboutus__button');
 const hiddenInfos = document.querySelectorAll('.container__info--hidden');
 const hiddenInfoSpan = document.querySelector('.container__info span');
@@ -25,5 +27,86 @@ window.addEventListener('resize', () => {
     hiddenInfoSpan.style.display = 'initial';
   } else if (window.screen.width < 767 && buttonMore.textContent === 'Подробнее') {
     hiddenInfoSpan.style.display = 'none';
+  }
+});
+
+// Footer
+
+const footerTitle2 = document.querySelector('.footer__title2');
+const footerTitle3 = document.querySelector('.footer__title3');
+
+const openDescription = (el) => {
+  el.classList.toggle('title-closed');
+  el.style.borderBottom = 'none';
+  el.nextElementSibling.style.display = 'flex';
+  el.nextElementSibling.style.borderBottom = '1px solid rgba(255, 255, 255, 0.7)';
+};
+
+const closeDescription = (el) => {
+  el.classList.toggle('title-closed');
+  el.style.borderBottom = '1px solid rgba(255, 255, 255, 0.7)';
+  el.nextElementSibling.style.display = 'none';
+  el.nextElementSibling.style.borderBottom = 'none';
+};
+
+footerTitle2.addEventListener('click', () => {
+  if (window.screen.width < 770) {
+    if (footerTitle2.classList.contains('title-closed')) {
+      openDescription(footerTitle2);
+      closeDescription(footerTitle3);
+    } else {
+      openDescription(footerTitle3);
+      closeDescription(footerTitle2);
+    }
+  }
+});
+
+footerTitle2.addEventListener('keypress', (evt) => {
+  if (evt.keyCode === 13 && window.screen.width < 770) {
+    if (footerTitle2.classList.contains('title-closed')) {
+      openDescription(footerTitle2);
+      closeDescription(footerTitle3);
+    } else {
+      openDescription(footerTitle3);
+      closeDescription(footerTitle2);
+    }
+  }
+});
+
+footerTitle3.addEventListener('click', () => {
+  if (window.screen.width < 770) {
+    if (footerTitle2.classList.contains('title-closed')) {
+      openDescription(footerTitle2);
+      closeDescription(footerTitle3);
+    } else {
+      openDescription(footerTitle3);
+      closeDescription(footerTitle2);
+    }
+  }
+});
+
+footerTitle3.addEventListener('keypress', (evt) => {
+  if (evt.keyCode === 13 && window.screen.width < 770) {
+    if (footerTitle2.classList.contains('title-closed')) {
+      openDescription(footerTitle2);
+      closeDescription(footerTitle3);
+    } else {
+      openDescription(footerTitle3);
+      closeDescription(footerTitle2);
+    }
+  }
+});
+
+window.addEventListener('resize', () => {
+  if (window.screen.width > 770) {
+    footerTitle2.style.borderBottom = 'none';
+    footerTitle3.style.borderBottom = 'none';
+    footerTitle2.nextElementSibling.style.display = 'flex';
+    footerTitle3.nextElementSibling.style.display = 'flex';
+    footerTitle2.nextElementSibling.style.borderBottom = 'none';
+    footerTitle3.nextElementSibling.style.borderBottom = 'none';
+  } else {
+    openDescription(footerTitle3);
+    closeDescription(footerTitle2);
   }
 });
